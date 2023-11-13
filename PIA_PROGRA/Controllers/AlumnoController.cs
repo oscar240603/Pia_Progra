346ba5df55668e166f31cdb5fe77283095d5e46d
oscar240603 <oscaryael12385@gmail.com>
2023-11-13 01:49:33 -0600

    Carpetas creadas

---
+++ PIA_PROGRA/Controllers/AlumnoController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PIA_PROGRA.Controllers
+{
+    [Route("api/[Controller]")]
+    [ApiController]
+    public class AlumnoController : ControllerBase
+    {
+        [HttpPost]
+        [Route("Guardar")]
+
+        public IActionResult Guardar()
+        {
+
+        }
+
+    }
+}
