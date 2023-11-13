6ab10abc5b2dc17809e9a06d253d9e183d14583f
MonseHernandez4 <monserrat.hernandezen@uanl.edu.mx>
2023-11-13 04:50:57 -0600

    Validar y GuardarInfo

--- BLL/BL_Alumno.cs
+++ BLL/BL_Alumno.cs
@@ -1,7 +1,9 @@
-﻿using System.Data;
-using System.Data.Common;
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Data;
 using System.Data.SqlClient;
-using BLL;
+using System.Linq;
 using DAL;
 using Entity;
 
@@ -9,7 +11,107 @@ namespace BLL
 {
     public class BL_Alumno
     {
-        public static List<string> EditarInfo(string Cadena, DtoAlumno alumno)
+        public static List<string> Validar(string PCadena, DtoAlumno alumno)
+        {
+            List<string> lstValidaciones = new List<string>();
+
+            if (ValidacionTexto(alumno.Nombre))
+            {
+                lstValidaciones.Add("Revise el Nombre del alumno");
+            }
+
+            if (ValidaNombreAlumno(PCadena, alumno.Nombre + alumno.Apaterno + alumno.Amaterno))
+            {
+                lstValidaciones.Add("El alumno ya fue registrado ");
+            }
+
+            if (ValidacionTexto(alumno.Apaterno))
+            {
+                lstValidaciones.Add("Revise el Apellido paterno");
+            }
+
+            if (ValidacionTexto(alumno.Amaterno))
+            {
+                lstValidaciones.Add("Revise el Apellido materno");
+            }
+
+            if (ValidacionTexto(alumno.Correo_Uni))
+            {
+                lstValidaciones.Add("Revise el Correo Universitario");
+            }
+
+            //if (ValidacionTexto(alumno.IdMatricula))
+            //{
+            //    lstValidaciones.Add("Revise la Matrícula");
+            //}
+            //Aqui marca error porque no seria ValidacionTexto sino que hay que hacer uno de Numero//
+
+            return lstValidaciones;
+        }
+
+        private static Boolean ValidaNombreAlumno(string PCadena, String alumno)
+        {
+            bool Validacion = false;
+
+            var dpParametros = new
+            {
+                P_Accion = 3,
+                P_Texto = alumno
+            };
+
+            DataTable Dt = Contexto.Funcion_StoreDB(PCadena, "Buscar", dpParametros);
+            if (Dt.Rows.Count > 0)
+            {
+                Validacion = true;
+
+            }
+            return Validacion;
+        }
+        //Buscar es Consulta?
+
+        private static bool ValidacionTexto(string PTexto)
+        {
+            bool Validacion = false;
+
+            foreach (char Letra in PTexto.Replace(" ", ""))
+            {
+                if (!char.IsLetter(Letra))
+                {
+                    Validacion = true;
+                    break;
+                }
+            }
+            return Validacion;
+        }
+
+        public static List<string> GuardarInfo(string Cadena, DtoAlumno alumno)
+        {
+            List<string> lstDatos = new List<string>();
+            try
+            {
+                var dpParametros = new
+                {
+                    P_Nombre = alumno.Nombre,
+                    P_APaterno = alumno.Apaterno,
+                    P_AMaterno = alumno.Amaterno,
+                    p_Correo_Uni = alumno.Correo_Uni,
+                    P_IdMatricula = alumno.IdMatricula
+                };
+
+                Contexto.Procedimiento_StoreDB(Cadena, "registrar", dpParametros);
+
+                lstDatos.Add("00");
+                lstDatos.Add("El alumno fue registrado con éxito");
+            }
+            catch (SqlException ex)
+            {
+                lstDatos.Add("14");
+                lstDatos.Add(ex.Message);
+            }
+            return lstDatos;
+        }
+
+        public static List<string> Editar(string Cadena, DtoAlumno alumno)
         {
             List<string> lstDatos = new List<string>();
             try
@@ -23,7 +125,7 @@ namespace BLL
                     P_IdMatricula= alumno.IdMatricula
                 };
 
-                Contexto.Procedimiento_StoreDB(Cadena, "Modificar", dpParametros);
+                Contexto.Procedimiento_StoreDB(Cadena, "modificar", dpParametros);
 
                 lstDatos.Add("00");
                 lstDatos.Add("El alumno fue modificado con éxito");
